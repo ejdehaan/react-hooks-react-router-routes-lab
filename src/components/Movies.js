bc88b2a37263a4571ff999ca5a4d52101bee528d
Emily Joy DeHaan <emilydehaan001@gmail.com>
2022-05-22 18:55:46 -0400

    Passed tests

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,28 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  
+
+  const renderMovies = movies.map(movie => (
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <p>Runtime={movie.time}</p>
+      <ul>
+        {movie.genres.map(genre => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      <div >
+        {renderMovies}
+      </div>
+    </div>
+  )
 }
 
 export default Movies;
